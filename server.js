e95875407cc2c07639beff90e79ee3005d7ae217
SadlerJ <28812304+sadlerjames@users.noreply.github.com>
2019-04-20 12:34:17 +0100

    added base files

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const port = 3000;
+const apiKey = "8e8313b2aad0769023ba8069ce3f5133";
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let unit = "metric";
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`
+  request(url, function (err, response, body) {
+    if (err) {
+      res.render('index', { weather: null, error: 'There was an error in getting your data, please try again!' });
+    } else {
+      let weather = JSON.parse(body)
+      console.log(weather);
+      if (weather.main == undefined) {
+        res.render('index', {weather: null, error: 'There was an error in getting the temperature for that specific city, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+})
